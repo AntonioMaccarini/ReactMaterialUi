39dce7b92efa9ffd55f802582a4787bd1b315e7e
Antonio Augusto Maccarini <amaccarini@weg.net>
2022-06-03 14:26:19 -0300

    34:00

--- src/components/NewComponents/Navbar.js
+++ src/components/NewComponents/Navbar.js
@@ -1,5 +1,6 @@
-import { Pets } from '@mui/icons-material'
-import { AppBar, Toolbar, styled, Typography } from '@mui/material'
+import { Mail, Notifications, Pets } from '@mui/icons-material'
+import { AppBar, Toolbar, styled, Typography, Box, InputBase, Badge, Avatar } from '@mui/material'
+import { width } from '@mui/system'
 import React from 'react'
 
 const StyledToolbar = styled(Toolbar)({
@@ -7,12 +8,33 @@ const StyledToolbar = styled(Toolbar)({
     justifyContent: "space-between"
 })
 
+const Search = styled("div")(({ theme }) => ({
+    backgroundColor: "white",
+    padding:"0 10px",
+    borderRadius: theme.shape.borderRadius,
+    width: "40%"
+}))
+
+const Icons = styled(Box)(({ theme }) => ({
+    backgroundColor: "white"
+}))
+
 const Navbar = () => {
   return (
     <AppBar position='stick'>
         <StyledToolbar>
         <Typography variant="h6" sx ={{display:{xs:"none", sm:"block"}}}>LAMA DEV</Typography>
         <Pets sx ={{display:{xs:"block", sm:"none"}}}/>
+        <Search><InputBase/></Search>
+        <Icons>
+            <Badge badgeContent={4} color="error">
+                <Mail/>
+            </Badge>
+            <Badge badgeContent={4} color="error">
+                <Notifications/>
+            </Badge>
+            <Avatar/>
+        </Icons>
         </StyledToolbar>
         
     </AppBar>
