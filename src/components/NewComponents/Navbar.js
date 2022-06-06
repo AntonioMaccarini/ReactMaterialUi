193083452e86b6f278c340722dddf97da62162c2
Antonio Augusto Maccarini <amaccarini@weg.net>
2022-06-06 08:57:38 -0300

    43:30

--- src/components/NewComponents/Navbar.js
+++ src/components/NewComponents/Navbar.js
@@ -1,7 +1,6 @@
 import { Mail, Notifications, Pets } from '@mui/icons-material'
-import { AppBar, Toolbar, styled, Typography, Box, InputBase, Badge, Avatar } from '@mui/material'
-import { width } from '@mui/system'
-import React from 'react'
+import { AppBar, Toolbar, styled, Typography, Box, InputBase, Badge, Avatar, Menu, MenuItem } from '@mui/material'
+import React, { useState } from 'react'
 
 const StyledToolbar = styled(Toolbar)({
     display: "flex",
@@ -16,26 +15,76 @@ const Search = styled("div")(({ theme }) => ({
 }))
 
 const Icons = styled(Box)(({ theme }) => ({
-    backgroundColor: "white"
+    display:'none',
+    gap: "20px",
+    alignItems: "center",
+    [theme.breakpoints.up("sm")]:{
+        display: "flex"
+    }
+}))
+
+const UserBox = styled(Box)(({ theme }) => ({
+    display:'flex',
+    gap: "10px",
+    alignItems: "center",
+    [theme.breakpoints.up("sm")]:{
+        display: "flex"
+    }
 }))
 
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false)
+
+    const handleOnAvatarClick = () => {
+        setOpen(true)
+    }
+
+ 
   return (
     <AppBar position='stick'>
         <StyledToolbar>
-        <Typography variant="h6" sx ={{display:{xs:"none", sm:"block"}}}>LAMA DEV</Typography>
-        <Pets sx ={{display:{xs:"block", sm:"none"}}}/>
-        <Search><InputBase/></Search>
-        <Icons>
-            <Badge badgeContent={4} color="error">
-                <Mail/>
-            </Badge>
-            <Badge badgeContent={4} color="error">
-                <Notifications/>
-            </Badge>
-            <Avatar/>
-        </Icons>
+            <Typography variant="h6" sx ={{display:{xs:"none", sm:"block"}}}>LAMA DEV</Typography>
+            <Pets sx ={{display:{xs:"block", sm:"none"}}}/>
+            <Search>
+                <InputBase placeholder="search..."/>
+            </Search>
+            <Icons>
+                <Badge badgeContent={4} color="error">
+                    <Mail/>
+                </Badge>
+                <Badge badgeContent={4} color="error">
+                    <Notifications/>
+                </Badge>
+                <UserBox onClick={handleOnAvatarClick}>
+                <Avatar
+                    sx={{width:30, height:30}} 
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvLZg6pSN9NTAuF7_rQ9Fj9g6EeWMzH3O-XQ&usqp=CAU"
+                    
+                />
+                <Typography variant="span">Antonio</Typography>
+            </UserBox>
+            </Icons>
+            
         </StyledToolbar>
+        <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        >
+            <MenuItem>Profile</MenuItem>
+            <MenuItem>My account</MenuItem>
+            <MenuItem>Logout</MenuItem>
+      </Menu>
         
     </AppBar>
   )
--- src/components/NewComponents/Sidebar.js
+++ src/components/NewComponents/Sidebar.js
@@ -1,9 +1,21 @@
-import { Box } from '@mui/material'
+import { Home } from '@mui/icons-material'
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import React from 'react'
 
 const Sidebar = () => {
   return (
-    <Box bgcolor="lightblue" flex ={1} p={2} sx={{display:{ xs: "none", sm: "block"}}}>Sidebar</Box>
+    <Box bgcolor="" flex ={1} p={2} sx={{display:{ xs: "none", sm: "block"}}}>
+      <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#home">
+              <ListItemIcon>
+                <Home/>
+              </ListItemIcon>
+              <ListItemText primary="Homepage" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+    </Box>
   )
 }
 
